dcb6e314c87b4ee9f10654acd7c53ee5b28ebf8b
Kuki031 <luka.kukucka1@gmail.com>
2024-09-11 17:16:22 +0200

    Initial commit

---
+++ src/main/java/com/abbrevio/abbrevio/AbbrevioApplication.java
@@ -0,0 +1,13 @@
+package com.abbrevio.abbrevio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbrevioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbrevioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abbrevio/abbrevio/AbbrevioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbrevio.abbrevio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbrevioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
