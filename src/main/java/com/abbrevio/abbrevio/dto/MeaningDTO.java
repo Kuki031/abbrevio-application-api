fe9a49f9645b61096405ce098e467b59c098cb9c
Kuki031 <luka.kukucka1@gmail.com>
2024-09-30 21:37:20 +0200

    Fetch comments along with meaning

--- src/main/java/com/abbrevio/abbrevio/dto/MeaningDTO.java
+++ src/main/java/com/abbrevio/abbrevio/dto/MeaningDTO.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,4 +21,5 @@ public class MeaningDTO {
     private Long countOfVotes;
     private UserDTO user;
     private Long abbreviationId;
+    private List<CommentDTO> comments;
 }
